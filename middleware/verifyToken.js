4946f057f5473bf360fa5c0961beb9b0181f35a2
Saikiran <kiranbrettlee418@gmail.com>
2023-03-28 23:51:48 +0530

    first commit

---
+++ middleware/verifyToken.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  console.log("----->>>>>>>" ,authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("----->>>>>>>" ,token);
+  if (token == null) return res.status(401).json("You are not authenticated!");
+
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });          
+  
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    //the user tying to edit must update   own profile or he must be admin
+    if (req.user.id === req.params.userId || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+//Only admin is allowed
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
